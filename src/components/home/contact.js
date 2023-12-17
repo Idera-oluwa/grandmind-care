1b0ef6b05649b30414fece29fcb638b031685b1f
Idera_oluwa <ideraoluwaisa2003@gmail.com>
2023-12-17 08:37:41 -0800

    responsiveness design added

--- src/components/home/contact.js
+++ src/components/home/contact.js
@@ -4,11 +4,13 @@ const contact = () => {
   return (
     <div className='mt-[5rem] relative pb-[5rem] h-[652px]'>
       <img src='Images/home/contact.png' alt='' className='object-cover rounded-[10px]'/>
-      <div className='absolute top-0 left-0 flex flex-col items-center w-full mt-[1rem] py-[1rem]'>
-      <p className='text-[24px] font-bold text-white'>Contact the team</p>
-      <p className='text-[14px] font-medium md:font-semibold text-white w-[95%] md:w-[80%] lg:w-[40%] text-center mt-[8px]'>Contacting the team is easy, enter your name, your email, and your message to get started.</p>
-      <div className='w-full md:w-[588px] h-[528px] rounded-[10px] bg-[#474973B2] mt-[1.5rem] flex items-center justify-center shadow-shadow2'>
-      <div className='w-full md:w-[557px] h-[500px] border border-[#424359] rounded-[10px] bg-white mt-[1.5rem] p-[2rem]'>
+      <div className='absolute top-0 left-0 flex flex-col items-center w-full mt-[1rem]'>
+      <div className='py-[1rem] md:py-0 text-center flex flex-col items-center w-full'>
+        <p className='text-[24px] font-bold text-white'>Contact the team</p>
+        <p className='text-[14px] font-medium md:font-semibold text-white w-[95%] md:w-[80%] lg:w-[40%] text-center mt-[8px]'>Contacting the team is easy, enter your name, your email, and your message to get started.</p>
+      </div>
+      <div className='w-full md:w-[588px] h-[528px] rounded-[10px] bg-[#474973B2] mt-[1rem] md:mt-[1.5rem] flex items-center justify-center shadow-shadow2'>
+      <div className='w-full md:w-[557px] h-[500px] border border-[#424359] rounded-[10px] bg-white md:mt-[1.5rem] p-[2rem]'>
          <form action="https://formspree.io/f/xyyrgynn"
   method="POST">
             <div className='flex flex-col gap-[10px]'>

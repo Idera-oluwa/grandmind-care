35d5825c148051073db0cc59c33cc978cf4de284
Idera_oluwa <ideraoluwaisa2003@gmail.com>
2023-12-20 05:57:04 -0800

    responsiveness changes added

--- src/components/staffing/specialized.js
+++ src/components/staffing/specialized.js
@@ -8,7 +8,7 @@ const specialized = () => {
        <p className='font-semibold text-[28px] md:text-[32px] lgtext-[36px] text-black'>Specialised Staffs</p>
        <img src='Images/staffing/specialized.png' alt='' className='grid lg:hidden w-full mt-[1rem]'/>
        <p className='font-normal text-[16px] text-[#1B1B1E] mt-[1rem]'>Our extensive staff database includes a diverse range of specialists, encompassing</p>
-       <div className='grid grid-cols-2 mt-[1rem] gap-[1rem]'>
+       <div className='grid grid-cols-1 md:grid-cols-2 mt-[1rem] gap-[1rem]'>
          <div className='p-[2rem] border border-[#9C9EBF80] rounded-[10px]'>
            <div className='w-full h-[11px] rounded-[20px] bg-[#FFD166]'></div>
            <div className='flex flex-col mt-[1rem] gap-[0.5rem]'>

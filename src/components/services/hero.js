7cd339032e5525d38463d88b3fadc81c0a0e3acf
Idera_oluwa <ideraoluwaisa2003@gmail.com>
2023-12-20 03:28:37 -0800

    responsiveness changes added

--- src/components/services/hero.js
+++ src/components/services/hero.js
@@ -3,14 +3,14 @@ import Link from 'next/link';
 
 const hero = () => {
   return (
-    <div className='flex flex-row items-center'>
-     <div className='w-[50%]'>
-      <h1 className='font-bold text-[40px] text-[#474973]'>Our <span className='font-bold text-[#1B1B1E]'>Comprehensive</span></h1>
-      <h1 className='font-bold text-[40px] text-[#474973]'><span className='font-bold text-[#1B1B1E]'>Care</span> Services</h1>
+    <div className='flex flex-col md:flex-row items-center'>
+     <div className='w-full md:w-[50%]'>
+      <h1 className='font-bold text-[32px] md:text-[35px] lg:text-[40px] text-[#474973]'>Our <span className='font-bold text-[#1B1B1E]'>Comprehensive</span></h1>
+      <h1 className='font-bold text-[32px] md:text-[35px] lg:text-[40px] text-[#474973]'><span className='font-bold text-[#1B1B1E]'>Care</span> Services</h1>
       <p className='text-[16px] font-normal text-[#1B1B1E] leading-[26.33px] w-[90%] mt-[1rem]'>At Grandmind Care, we understand that each individual has unique needs, and we are dedicated to providing personalised and compassionate care. Our range of care services is designed to enhance the well-being and quality of life for your loved ones.</p>
       <Link href='/about#register'><button className='bg-[#474973] rounded-[22px] py-[12px] px-[20px] text-[15px] font-bold text-[#FDFDFD] tracking-[1px] mt-[2rem] hover:text-black border border-transparent hover:border-[#1B1B1E] hover:bg-transparent transition ease-in-out delay-300'>Register today</button></Link>
      </div>
-     <img src='Images/services/our-services-image.png' alt=''/>
+     <img src='Images/services/our-services-image.png' alt='' className='w-full md:w-[50%] mt-[3rem] md:mt-full'/>
     </div>
   )
 }
--- src/components/services/how-we-work.js
+++ src/components/services/how-we-work.js
@@ -2,16 +2,16 @@ import React from 'react'
 
 const howWeWork = () => {
   return (
-    <div className='mt-[8rem] bg-[#252648] w-[90%] mx-auto flex flex-row relative h-[312px]'>
-       <div className='w-[40%]'>
-        <img src='Images/services/care4.png' alt='' className='absolute top-0 mt-[-3rem] left-0 ml-[3rem]' />
-         <p className='font-medium text-[12px] text-[#F6F8FF] mt-[11rem] ml-[3rem]'>We conduct a thorough assessment to understand your loved one's unique needs. Based on the assessment, we create a personalised care plan tailored to individual requirement</p>
-         <p className='font-medium text-[12px] text-[#F6F8FF] mt-[1rem] ml-[3rem]'>Our team is comprised of experienced caregivers who bring expertise and compassion to every interaction.</p>
+    <div className='mt-[8rem] bg-[#252648] w-[90%] mx-auto flex flex-col-reverse lg:flex-row relative h-auto py-[2rem] lg:py-0 rounded-[10px]'>
+       <div className='w-full lg:w-[40%]'>
+        <img src='Images/services/care4.png' alt='' className='hidden lg:grid lg:absolute top-0 mt-[-3rem] left-0 ml-[3rem]' />
+         <p className='font-medium text-[12px] text-[#F6F8FF] mt-[2rem] lg:mt-[11rem] px-[1rem] lg:ml-[3rem]'>We conduct a thorough assessment to understand your loved one's unique needs. Based on the assessment, we create a personalised care plan tailored to individual requirement</p>
+         <p className='font-medium text-[12px] text-[#F6F8FF] mt-[1rem] px-[1rem] lg:ml-[3rem]'>Our team is comprised of experienced caregivers who bring expertise and compassion to every interaction.</p>
        </div>
-       <div className='w-[40%] ml-auto mr-[3rem] flex flex-col items-center'>
+       <div className='w-full lg:w-[40%] ml-auto mr-[3rem] flex flex-col items-center'>
         <p className='font-extrabold text-[24px] text-white mt-[2rem] text-center'>How  We Work</p>
         <p className='font-semibold text-[12px] text-[#F6F8FF] mt-[1rem] text-center w-[356px]'>From initial assessments to ongoing support, our process is designed to ensure the best possible care</p>
-        <img src='Images/services/care5.png' alt='' className='mt-[1rem]'/>
+        <img src='Images/services/care5.png' alt='' className='mt-[1rem] w-[95%] lg:w-auto mx-auto'/>
        </div>
     </div>
   )

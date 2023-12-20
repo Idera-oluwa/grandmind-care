176b2e4e43f9dfa83343064932cd3e46cb8e2be9
Idera_oluwa <ideraoluwaisa2003@gmail.com>
2023-12-20 03:45:41 -0800

    responsiveness changes added

--- src/components/services/care-plans.js
+++ src/components/services/care-plans.js
@@ -7,21 +7,21 @@ const carePlans = () => {
         <div className='w-full md:w-[50%]'>
             <p className='font-semibold text-[32px] md:text-[35px] lg:text-[40px] text-[#1B1B1E]'>Personalised</p>
             <p className='font-semibold text-[32px] md:text-[35px] lg:text-[40px] text-[#1B1B1E]'>Care Plans</p>
-            <p className='text-[16px] font-normal w-[90%] mt-[1rem]'>We offer respite care services to provide temporary relief for family caregivers. This allows you to take a break while ensuring your loved ones receive the care they need.</p>
+            <p className='text-[16px] font-normal w-full lg:w-[90%] mt-[1rem]'>We offer respite care services to provide temporary relief for family caregivers. This allows you to take a break while ensuring your loved ones receive the care they need.</p>
         </div>
         <img src='Images/services/care1.png' alt='' className='w-full md:w-[50%]'/>
       </div>
       <div className='flex flex-col md:flex-row-reverse items-center gap-[2rem]'>
         <div className='w-full md:w-[50%]'>
             <p className='font-semibold text-[32px] md:text-[35px] lg:text-[40px] text-[#1B1B1E]'>Specialised Care</p>
-            <p className='text-[16px] font-normal w-[90%] mt-[1rem]'>We offer respite care services to provide temporary relief for family caregivers. This allows you to take a break while ensuring your loved ones receive the care they need.</p>
+            <p className='text-[16px] font-normal w-full lg:w-[90%] mt-[1rem]'>We offer respite care services to provide temporary relief for family caregivers. This allows you to take a break while ensuring your loved ones receive the care they need.</p>
         </div>
         <img src='Images/services/care2.png' alt='' className='w-full md:w-[50%]'/>
       </div>
       <div className='flex flex-col md:flex-row items-center gap-[2rem] md:gap-0'>
         <div className='w-full md:w-[50%]'>
             <p className='font-semibold text-[32px] md:text-[35px] lg:text-[40px] text-[#1B1B1E]'>Respite Care</p>
-            <p className='text-[16px] font-normal w-[90%] mt-[1rem]'>We offer respite care services to provide temporary relief for family caregivers. This allows you to take a break while ensuring your loved ones receive the care they need.</p>
+            <p className='text-[16px] font-normal w-full lg:w-[90%] mt-[1rem]'>We offer respite care services to provide temporary relief for family caregivers. This allows you to take a break while ensuring your loved ones receive the care they need.</p>
         </div>
         <img src='Images/services/care3.png' alt='' className='w-full md:w-[50%]'/>
       </div>

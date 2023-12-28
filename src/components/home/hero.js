bad96273dbe8140907c034ee274d4af3b82bcbfc
Idera_oluwa <ideraoluwaisa2003@gmail.com>
2023-12-27 22:06:16 -0800

    changes added

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -23,16 +23,16 @@ const hero = () => {
           width={18}
           height={16}
            />
-         <a href="mailto:info@grandmind.co.uk"><p className='text-[10px] md:text-[14px] font-normal text-[#474973]'>info@grandmind.co.uk</p></a>
+         <a href="mailto:info@grandmind.co.uk"><p className='text-[13px] md:text-[14px] font-normal text-[#474973]'>info@grandmind.co.uk</p></a>
        </div>
        <div className='flex flex-row gap-[8px] items-center cursor-pointer'>
          <img src='Images/home/whatsapp-icon.png' alt='' className='w-[17px] h-[17px]'/>
-         <a href='https://wa.me/447903220307' target="_blank" rel="noreferrer"><p className='text-[10px] md:text-[14px] font-normal text-[#474973]'>07903220307</p></a>
+         <a href='https://wa.me/447903220307' target="_blank" rel="noreferrer"><p className='text-[13px] md:text-[14px] font-normal text-[#474973]'>07903220307</p></a>
        </div>
        </div>
        <h1 className='text-[32px] md:text-[35px] lg:text-[40px] font-bold leading-[48.41px] text-[#1B1B1E] mt-[1.5rem]'><span className='text-[#474973]'>Welcome</span> To <span className='text-[#474973]'>Your</span></h1>
        <i className='text-[32px] md:text-[35px] lg:text-[40px] font-bold leading-[48.41px] text-[#1B1B1E] mt-[48px]'>No 1 Carer <span className='text-[#474973]'>Service</span> </i>
-       <p className='text-[11px] md:text-[14px] leading-[25.6px] text-[#1B1B1E] font-normal w-[95%] md:w-[80%] mt-[0.5rem] md:mt-[2rem]'>We're delighted to welcome you to <span className='text-[#474973] font-semibold'>Grandmind Care LTD</span>, where compassionate care meets cutting-edge convenience. Here, you’ll find a supportive community,valuable resources, and a seamless interaction with our professional carers.</p>
+       <p className='text-[14px] md:text-[14px] leading-[25.6px] text-[#1B1B1E] font-normal w-[95%] md:w-[80%] mt-[0.5rem] md:mt-[2rem]'>We're delighted to welcome you to <span className='text-[#474973] font-semibold'>Grandmind Care LTD</span>, where compassionate care meets cutting-edge convenience. Here, you’ll find a supportive community,valuable resources, and a seamless interaction with our professional carers.</p>
        <div className='flex flex-row mt-[1.5rem] gap-[10px]'>
        <Link href='/about#register'><button className='bg-[#474973] rounded-[22px] py-[9px] lg:py-[10px] px-[15px] lg:px-[18px] text-[15px] font-bold text-[#FDFDFD] tracking-[1px] border border-transparent hover:bg-[hsl(237,24%,25%)] transition ease-in-out delay-500'>Register today</button></Link>
         <Link href='/about'> <button className='hidden md:block bg-transparent rounded-[22px] py-[9px] lg:py-[10px] px-[15px] lg:px-[18px] text-[15px] font-medium text-black border border-[#1B1B1E] tracking-[1px] hover:bg-[#474973] hover:border-transparent hover:text-[#FDFDFD] transition ease-in-out delay-500'>Learn more</button> </Link>
@@ -65,7 +65,7 @@ const hero = () => {
       </SwiperSlide>
     </Swiper>
        <h1 className='text-[20px] font-semibold text-[#1B1B1E] text-center md:text-left mt-[1.5rem] md:mt-[1rem]'>Our <span className='text-[20px] font-bold text-[#E27396] mt-[1rem]'>Mission</span></h1>
-       <p className='text-[11px] md:text-[14px] font-medium text-[#1B1B1E] mt-[0.5rem] md:mt-0 text-center md:text-left'>is to make this journey smoother for both caregivers and those in need of care. Here, you'll find a supportive community, valuable resources, and a seamless</p>
+       <p className='text-[14px] md:text-[14px] font-medium text-[#1B1B1E] mt-[0.5rem] md:mt-0 text-center md:text-left'>is to make this journey smoother for both caregivers and those in need of care. Here, you'll find a supportive community, valuable resources, and a seamless</p>
      </div>
     </div>
   )

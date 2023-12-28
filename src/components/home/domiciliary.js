0d1778455cdb8c4568b13808b131ea333608dcf8
Idera_oluwa <ideraoluwaisa2003@gmail.com>
2023-12-27 22:28:03 -0800

    changes added

--- src/components/home/domiciliary.js
+++ src/components/home/domiciliary.js
@@ -13,7 +13,7 @@ import 'swiper/css/effect-fade';
 const domiciliary = () => {
   const sliderRef = useRef();
   return (
-    <div className='mt-[10rem]'>
+    <div className='mt-[7rem] md:mt-[10rem]'>
      <h1 className='text-[20px] font-semibold text-[#1B1B1E] text-center'>Domiciliary Care Services</h1>
      <div className='mt-[2rem] relative h-[290px] md:h-[412px] w-[100%]'>
         <img src='Images/home/care-img.png' alt='' className='absolute z-10 w-full h-full object-cover'/>
@@ -46,7 +46,7 @@ const domiciliary = () => {
           <div className='flex flex-col ml-0 md:ml-[3rem] w-[100%] md:w-[50%] mt-[1rem] md:mt-0'>
            <p className='font-medium text-[20px] md:text-[40px] text-[#FFFFFF] text-center md:text-left'>Dependable <span className='text-[#32936F]'>&</span> Reliable</p>
            <p className='font-normal text-[14px] text-[#FFFFFF] text-center md:text-left'>is to make this journey smoother for both caregivers and those in need of care.</p>
-           <Link href='/about'><button className='bg-[#474973] py-[5px] flex items-center justify-center rounded-[32px] text-[15px] font-medium text-[#F6F8FF] w-[122px] mt-[2rem] mt-[1rem] md:mt-[2rem] mx-auto md:ml-0'>Learn more</button></Link>
+           <Link href='/about'><button className='bg-[#474973] py-[5px] flex items-center justify-center rounded-[32px] text-[15px] font-medium text-[#F6F8FF] w-[122px] mt-[1rem] md:mt-[2rem] mx-auto md:ml-0'>Learn more</button></Link>
           </div>
         </div>
         </SwiperSlide>
@@ -56,7 +56,7 @@ const domiciliary = () => {
           <div className='flex flex-col ml-0 md:ml-[3rem] w-[100%] md:w-[50%] mt-[1rem] md:mt-0'>
            <p className='font-medium text-[20px] md:text-[40px] text-[#FFFFFF] text-center md:text-left'>Personalised <span className='text-[#32936F]'>Care</span></p>
            <p className='font-normal text-[14px] text-[#FFFFFF] text-center md:text-left'>is to make this journey smoother for both caregivers and those in need of care.</p>
-           <Link href='/about'><button className='bg-[#474973] py-[5px] flex items-center justify-center rounded-[32px] text-[15px] font-medium text-[#F6F8FF] w-[122px] mt-[2rem] mt-[1rem] md:mt-[2rem] mx-auto md:ml-0'>Learn more</button></Link>
+           <Link href='/about'><button className='bg-[#474973] py-[5px] flex items-center justify-center rounded-[32px] text-[15px] font-medium text-[#F6F8FF] w-[122px] mt-[1rem] md:mt-[2rem] mx-auto md:ml-0'>Learn more</button></Link>
           </div>
         </div>
         </SwiperSlide>
--- src/components/home/why-us.js
+++ src/components/home/why-us.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const WhyChooseUs = () => {
   return (
-    <div className='mt-[10rem]'>
+    <div className='mt-[7rem] md:mt-[10rem]'>
       <h1 className='text-[20px] font-semibold text-[#1B1B1E] text-center'>Why Choose Us</h1>
       <div className='mt-[2rem]'>
         <div className='bg-[#474973D4] w-full hidden md:grid grid-cols-3 py-[1rem] rounded-[7px]'>
@@ -13,16 +13,16 @@ const WhyChooseUs = () => {
         </div>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-[3rem] md:gap-0 md:mt-[1rem]'>
         <div className='col-span-1 md:px-[2rem]'>
-        <img src='Images/home/patient1.png' alt='' className='w-full h-[23rem] md:hidden object-cover rounded-[10px]'/>
-         <p className='text-[18px] md:text-[20px] font-semibold text-[#1B1B1E] mt-[0.5rem]'>Personalised Care Plans</p>
-         <p className='text-[14px] md:text-[16px] font-medium text-[#1B1B1E] leading-[21.78px] mt-[1.5rem]'>We design individualized care plans to ensure your loved ones receive the attention and support they deserve</p>
+        <img src='Images/home/patient1.png' alt='' className='w-full h-[209px] md:hidden object-cover rounded-[10px]'/>
+         <p className='text-[18px] md:text-[20px] font-semibold text-[#1B1B1E] mt-[1.5rem] md:mt-[0.5rem]'>Personalised Care Plans</p>
+         <p className='text-[14px] md:text-[16px] font-medium text-[#1B1B1E] leading-[21.78px] mt-[1rem] md:mt-[1.5rem]'>We design individualized care plans to ensure your loved ones receive the attention and support they deserve</p>
          <div className='flex flex-row items-center justify-between mt-[1.5rem]'>
          <Link href='/about'><button className='py-[5px] px-[20px] border border-[#1B1B1E] text-[#1B1B1E] rounded-[32px] hover:bg-[#474973] hover:border-transparent hover:text-[#FDFDFD] transition ease-in-out delay-500'>Learn more</button></Link>
          <img src='Images/home/Grandmind-care.png' alt='' className='w-[38px] h-[38px] mr-[1rem]'/>
          </div>
         </div>
         <div className='col-span-1 md:px-[2rem]'>
-        <img src='Images/home/patient2.png' alt='' className='w-full h-[23rem] md:hidden object-cover rounded-[10px]'/>
+        <img src='Images/home/patient2.png' alt='' className='w-full h-[209px] md:hidden object-cover rounded-[10px]'/>
          <p className='text-[18px] md:text-[20px] font-semibold text-[#1B1B1E] mt-[0.5rem]'>Skilled Professionals</p>
          <p className='text-[14px] md:text-[16px] font-medium text-[#1B1B1E] leading-[21.78px] mt-[1.5rem]'>Our experienced and trained caregivers are committed to providing quality assistance with a focus on dignity and respect.</p>
          <div className='flex flex-row items-center justify-between mt-[1.5rem]'>
@@ -31,7 +31,7 @@ const WhyChooseUs = () => {
          </div>
         </div>
         <div className='col-span-1 md:px-[2rem]'>
-        <img src='Images/home/patient3.png' alt='' className='w-full h-[23rem] md:hidden object-cover rounded-[10px]'/>
+        <img src='Images/home/patient3.png' alt='' className='w-full h-[209px] md:hidden object-cover rounded-[10px]'/>
          <p className='text-[18px] md:text-[20px] font-semibold text-[#1B1B1E] mt-[0.5rem]'>Comprehensive Support</p>
          <p className='text-[14px] md:text-[16px] font-medium text-[#1B1B1E] leading-[21.78px] mt-[1.5rem]'>We design individualized care plans to ensure your loved ones receive the attention and support they deserve</p>
          <div className='flex flex-row items-center justify-between mt-[1.5rem]'>

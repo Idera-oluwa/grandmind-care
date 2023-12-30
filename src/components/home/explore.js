53a140ecae1af6b02126f40f2c528125fdd84e60
Auf <azeezumarfaruk@gmail.com>
2023-12-30 13:34:44 +0100

    feat

--- src/components/home/explore.js
+++ src/components/home/explore.js
@@ -1,13 +1,12 @@
-"use client"
+"use client";
 import React from "react";
 import Link from "next/link";
-import { Slide,Fade  } from "react-awesome-reveal";
+import { Slide, Fade } from "react-awesome-reveal";
 
 const explore = () => {
   return (
     <div className="flex flex-row mt-[7rem] md:mt-[10rem] items-center">
       <div className="w-full md:w-[50%]">
-      <Slide direction="up">
         <p className="text-[32px] lg:text-[40px] font-bold text-black leading-[55.55px]">
           Explore our flexible
         </p>
@@ -29,9 +28,13 @@ const explore = () => {
           <button className="py-[5px] px-[20px] border border-[#1B1B1E] text-[#1B1B1E] rounded-[32px] mt-[2rem] hover:bg-[#474973] hover:border-transparent hover:text-[#FDFDFD] transition-all ease-out duration-[600]">
             Learn more
           </button>
-        </Link></Slide>
+        </Link>
       </div>
-      <div className="w-0 md:w-[50%]"><Slide direction="up"><img src="Images/home/staffing.png" alt="" className="w-full" /></Slide></div>
+      <div className="w-0 md:w-[50%]">
+        <Slide direction="up">
+          <img src="Images/home/staffing.png" alt="" className="w-full" />
+        </Slide>
+      </div>
     </div>
   );
 };

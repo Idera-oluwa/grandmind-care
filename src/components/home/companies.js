aa23fed0cc93613be5c42f5860beaa6c1bba086f
Idera_oluwa <ideraoluwaisa2003@gmail.com>
2024-01-02 03:38:50 -0800

    marquee changed

--- src/components/home/companies.js
+++ src/components/home/companies.js
@@ -5,14 +5,14 @@ import Marquee from "react-fast-marquee"
 const companies = () => {
 
   return (
-    <div className='mt-[10rem]'>
+    <div className='mt-[10rem] w-full'>
         <Marquee>
-    <img src='Images/home/logo-1.svg' alt=''/>
-    <img src='Images/home/logo-2.svg' alt=''/>
-    <img src='Images/home/logo-3.svg' alt=''/>
-    <img src='Images/home/logo-1.svg' alt=''/>
-    <img src='Images/home/logo-2.svg' alt=''/>
-    <img src='Images/home/logo-3.svg' alt=''/>
+    <img src='Images/home/logo-1.svg' alt='' className='lg:mr-[5rem]'/>
+    <img src='Images/home/logo-2.svg' alt='' className='lg:mr-[5rem]'/>
+    <img src='Images/home/logo-3.svg' alt='' className='lg:mr-[5rem]'/>
+    <img src='Images/home/logo-1.svg' alt='' className='lg:mr-[5rem]'/>
+    <img src='Images/home/logo-2.svg' alt='' className='lg:mr-[5rem]'/>
+    <img src='Images/home/logo-3.svg' alt='' className='lg:mr-[5rem]'/>
     </Marquee>
     </div>
   )

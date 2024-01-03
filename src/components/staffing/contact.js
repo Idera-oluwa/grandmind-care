2c9afe58af6a10a6b2af287f8eba0e1360565510
Idera_oluwa <ideraoluwaisa2003@gmail.com>
2024-01-03 13:14:19 -0800

    chang4s added to contact page

--- src/components/staffing/contact.js
+++ src/components/staffing/contact.js
@@ -9,14 +9,14 @@ const contact = () => {
     setFile(uploadedFile);
   };
   return (
-    <div className='mt-[7rem] md:mt-[12rem] max-w-[100vw] mb-[18rem]'>
+    <div className='mt-[7rem] md:mt-[12rem] max-w-[100vw] mb-[22rem]'>
       <h1 className='text-center text-[20px] font-semibold mb-[2rem]'>Apply Now</h1>
-      <div className='w-full bg-[#474973] h-[336px] flex flex-row px-[4rem]'>
-       <div className='w-[30%] my-[auto]'>
-        <h1 className='font-semibold text-[40px] text-[white]'>Tell us about you!</h1>
-        <p className='font-semibold text-[16px] text-[white] mt-[1rem]'>Get started with your application here, send a detailed copy of your cv to us and let us review your application.</p>
+      <div className='w-full bg-[#474973] h-[336px] flex flex-col lg:flex-row  px-[4rem]'>
+       <div className='w-full lg:w-[30%] my-[auto]'>
+        <h1 className='font-semibold md:text-[40px] text-[30px] text-[white]'>Tell us about you!</h1>
+        <p className='font-semibold md:text-[16px] text-[14px] text-[white] mt-[1rem]'>Get started with your application here, send a detailed copy of your cv to us and let us review your application.</p>
        </div>
-       <div className='w-[60%] ml-[auto]'>
+       <div className='w-full lg:w-[60%] ml-[auto]'>
        <div className='w-[600px] bg-[#474973] h-[300px] mt-[36px] border-b-0 border-2 border-white p-[1rem] rounded-t-[25px]'>
        <div className="w-full h-[528px] p-[1rem] rounded-[10px] bg-[#474973] items-center justify-center">
           <div className="w-full h-full border border-[#424359] rounded-[20px] bg-white p-[2rem]">
@@ -62,7 +62,7 @@ const contact = () => {
                   className="px-[20px] py-[20px] rounded-[10px] border border-[#47497380] h-[52px] outline-none"
                 />
               </div>
-              <label className='text-[14px] font-medium text-[#171C74] mt-[1rem]'> Click to upload your CV
+              <label className='text-[14px] font-medium text-[#171C74] mt-[1rem] cursor-pointer'> Click to upload your CV
                 <input
           type="file"
           className="hidden"

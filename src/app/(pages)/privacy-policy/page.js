ced990b8647df94852fd7408c0a9eb086f22c778
Idera_oluwa <ideraoluwaisa2003@gmail.com>
2024-01-06 21:54:53 -0800

    privacy policy page added

---
+++ src/app/(pages)/privacy-policy/page.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-[90vw] 2xl:max-w-[1440px] mx-auto mt-[4rem] mb-[5rem] overflow-hidden flex flex-col gap-[3rem]'>
+    <div>
+        <p className='font-medium text-[16px] md:text-[20px] text-[#64646F]'>Privacy Policy</p>
+        <p className='font-normal text-[14px] md:text-[16px] leading-[19.36px] mt-[1rem] text-[#64646F]'>Grandmind Care operates https://grandmindcare.co.uk/ and may operate other websites. It is Grandmind care policy to respect your privacy regarding any information we may collect while operating our websites</p>
+    </div>
+    <div>
+        <p className='font-medium text-[16px] md:text-[20px] text-[#64646F]'>Website Visitors</p>
+        <p className='font-normal text-[14px] md:text-[16px] leading-[19.36px] mt-[1rem] text-[#64646F]'>Like most website operators, Grandmind Care  collects non-personally-identifying information of the sort that web browsers and servers typically make available, such as the browser type, language preference, referring site, and the date and time of each visitor request.  Grandmind Care purpose in collecting non-personally identifying information is to better understand how  Grandmind Care  visitors use its website. From time to time, Grandmind Care may release non-personally-identifying information in the aggregate, e.g., by publishing a report on trends in the usage of its website.</p>
+        <p className='font-normal text-[14px] md:text-[16px] leading-[19.36px] mt-[1rem] text-[#64646F]'>Grandmind Care also collects potentially personally-identifying information like Internet Protocol (IP) addresses for logged in users and for users leaving comments on https://grandmindcare .co.uk/ blogs/sites. Grandmind Care only discloses logged in user and commenter IP addresses under the same circumstances that it uses and discloses personally-identifying information as described below, except that commenter IP addresses and email addresses are visible and disclosed to the administrators of the blog/site where the comment was left.</p>
+    </div>
+    <div>
+        <p className='font-medium text-[16px] md:text-[20px] text-[#64646F]'>Gathering of Personally-Identifying Information</p>
+        <p className='font-normal text-[14px] md:text-[16px] leading-[19.36px] mt-[1rem] text-[#64646F]'>Certain visitors to Grandmind Care websites choose to interact with Grandmind Care in ways that require Grandmind Care to gather personally-identifying information. The amount and type of information that Better Care Staffing gathers depends on the nature of the interaction. For example, we ask visitors who sign up at https://bettercarestaffing.co.uk/ to provide a username and email address. Those who engage in transactions with Grandmind Care are asked to provide additional information, including as necessary the personal and financial information required to process those transactions. In each case Grandmind Care collects such information only insofar as is necessary or appropriate to fulfill the purpose of the visitor’s interaction with Better Care Staffing. Grandmind Care does not disclose personally-identifying information other than as described below. And visitors can always refuse to supply personally-identifying information, with the caveat that it may prevent them from engaging in certain website-related activities.</p>
+    </div>
+    <div>
+        <p className='font-medium text-[16px] md:text-[20px] text-[#64646F]'>Aggregated Statistics</p>
+        <p className='font-normal text-[14px] md:text-[16px] leading-[19.36px] mt-[1rem] text-[#64646F]'>Grandmind Care may collect statistics about the behavior of visitors to its websites. Grandmind Care may display this information publicly or provide it to others. However, Grandmind Care does not disclose personally-identifying information other than as described below.</p>
+    </div>
+    <div>
+        <p className='font-medium text-[16px] md:text-[20px] text-[#64646F]'>Protection of Certain Personally-Identifying Information</p>
+        <p className='font-normal text-[14px] md:text-[16px] leading-[19.36px] mt-[1rem] text-[#64646F]'>Grandmind Care discloses potentially personally-identifying and personally-identifying information only to those of its employees, contractors and affiliated organizations that (i) need to know that information in order to process it on Grandmind Care behalf or to provide services available at Grandmind Care websites, and (ii) that have agreed not to disclose it to others. Some of those employees, contractors and affiliated organizations may be located outside of your home country; by using Grandmind Care websites, you consent to the transfer of such information to them. Grandmind Care will not rent or sell potentially personally-identifying and personally-identifying information to anyone. Other than to its employees, contractors and affiliated organizations, as described above, Grandmind Care discloses potentially personally-identifying and personally-identifying information only in response to a subpoena, court order or other governmental request, or when Grandmind Care believes in good faith that disclosure is reasonably necessary to protect the property or rights of Grandmind Care , third parties or the public at large. If you are a registered user of an Grandmind Care website and have supplied your email address, Grandmind Care may occasionally send you an email to tell you about new features, solicit your feedback, or just keep you up to date with what’s going on with Grandmind Care and our products. If you send us a request (for example via email or via one of our feedback mechanisms), we reserve the right to publish it in order to help us clarify or respond to your request or to help us support other users. Grandmind Care takes all measures reasonably necessary to protect against the unauthorized access, use, alteration or destruction of potentially personally-identifying and personally-identifying information</p>
+    </div>
+    <div>
+        <p className='font-medium text-[16px] md:text-[20px] text-[#64646F]'>Cookies</p>
+        <p className='font-normal text-[14px] md:text-[16px] leading-[19.36px] mt-[1rem] text-[#64646F]'>A cookie is a string of information that a website stores on a visitor’s computer, and that the visitor’s browser provides to the website each time the visitor returns. Grandmind Care  uses cookies to help Grandmind Care identify and track visitors, their usage of Grandmind Care website, and their website access preferences. Grandmind Care  visitors who do not wish to have cookies placed on their computers should set their browsers to refuse cookies before using Grandmind Care  websites, with the drawback that certain features of Grandmind Care websites may not function properly without the aid of cookies.</p>
+    </div>
+    <div>
+        <p className='font-medium text-[16px] md:text-[20px] text-[#64646F]'>Ads</p>
+        <p className='font-normal text-[14px] md:text-[16px] leading-[19.36px] mt-[1rem] text-[#64646F]'>Ads appearing on any of our websites may be delivered to users by advertising partners, who may set cookies. These cookies allow the ad server to recognize your computer each time they send you an online advertisement to compile information about you or others who use your computer. This information allows ad networks to, among other things, deliver targeted advertisements that they believe will be of most interest to you. This Privacy Policy covers the use of cookies by Grandmind Care and does not cover the use of cookies by any advertisers.</p>
+    </div>
+    <div>
+        <p className='font-medium text-[16px] md:text-[20px] text-[#64646F]'>Privacy Policy Changes</p>
+        <p className='font-normal text-[14px] md:text-[16px] leading-[19.36px] mt-[1rem] text-[#64646F]'>Although most changes are likely to be minor, Grandmind Care may change its Privacy Policy from time to time, and in Grandmind Care sole discretion. Grandmind Care encourages visitors to frequently check this page for any changes to its Privacy Policy. If you have a https://grandmindcare.co.uk/ account, you might also receive an alert informing you of these changes. Your continued use of this site after any change in this Privacy Policy will constitute your acceptance of such change.</p>
+    </div>
+    </div>
+  )
+}
+
+export default page

91c6ff4b93130d266e36be4dbfcaebc2ba0adb11
Idera_oluwa <ideraoluwaisa2003@gmail.com>
2024-02-10 22:49:50 -0800

    apply section edited

--- src/components/staffing/apply.js
+++ src/components/staffing/apply.js
@@ -4,9 +4,9 @@ const apply = () => {
   return (
     <div className='md:bg-[#2A2C58] w-full py-[3rem] mt-[7rem] md:mt-[10rem] relative mb-[7rem] md:mb-[12rem]' id="apply">
       <div className='w-[90vw] lg:w-[80vw]  2xl:max-w-[1440px] mx-auto md:flex md:flex-row items-center justify-between'>
-        <div className='md:w-[45%]'>
-            <p className='text-[24px] text-[#1B1B1E] md:text-[40px] md:text-[#FFFFFF] text-semibold leading-[48.41px] text-center md:text-left'>Ready to join us?</p>
-            <p className='text-[12px] md:text-[20px] text-[#1B1B1E] md:text-[#FFFFFF] text-normal leading-[24.2px] md:mt-[1rem] text-center md:text-left w-[90%] mx-auto md:w-full'>Get started by clicking the button below to send your CV and cover letter.</p>
+        <div className='md:w-[50%]'>
+            <p className='text-[24px] text-[#1B1B1E] md:text-[32px] md:text-[#FFFFFF] font-semibold leading-[48.41px] text-center md:text-left'>Ready to join us?</p>
+            <p className='text-[12px] md:text-[14px] text-[#1B1B1E] md:text-[#FFFFFF] font-normal leading-[16.9px] md:mt-[1rem] text-center md:text-left w-[90%] mx-auto md:w-full'>Click the button below to get started with your application to join us in any of the positions.</p>
             <a href="mailto:recruitment@grandmindcare.co.uk"><button className='bg-[#474973] rounded-[20px] h-[42px] w-[128px] hidden md:flex justify-center items-center text-[#F6F8FF] text-[15px] font-bold mt-[2rem]'>Apply Now</button></a>
             <div className='py-[1rem] px-[2rem] bg-[#FFFFFF] w-[336px] h-[241px] mt-[2rem] rounded-[10px] hidden md:block'>
             <div className="w-full h-[11px] rounded-[20px] bg-[#FFD166]"></div>
